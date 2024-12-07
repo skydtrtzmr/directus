8519fad875eea1313b916df6abb9614ca1fb098c
skydtrtzmr <2368178395@qq.com>
2024-12-07 18:00:41 +0800

    尝试写extension

---
+++ api/extensions/directus-extension-create-user/src/index.js
@@ -0,0 +1,25 @@
+
+export default ({ filter }, context) => {
+	const { services, getSchema } = context; // getSchema is a function that returns the schema of the current collection
+	const { ItemsService, UsersService, RolesService, PermissionsService } = services; // get the services we need
+
+	const functionName = async (input, meta, context) => {
+		console.log('Creating Student Item!');
+
+		input.number = 1706;
+
+		// const schema = await getSchema(); // 会报错
+		// const { accountability } = context;
+		// const itemsService = new ItemsService('students', { schema });
+
+		// const data = await itemsService.createOne({
+		// 	number: 22,
+		// 	email: 'hello@world.com',
+		// 	name: 'This is our first article',
+		// });
+
+		return input;
+	};
+
+	filter('students.items.create', functionName);
+};

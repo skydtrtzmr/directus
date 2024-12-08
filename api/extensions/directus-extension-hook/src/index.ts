21ae28103f57bd42ef6b0b5f9c1b8fb67eeeebf9
skydtrtzmr <2368178395@qq.com>
2024-12-08 12:52:07 +0800

    手写调用service的hook extension生效了！

--- api/extensions/directus-extension-hook/src/index.ts
+++ api/extensions/directus-extension-hook/src/index.ts
@@ -1,15 +1,58 @@
 import { defineHook } from '@directus/extensions-sdk';
+import type { HookExtensionContext } from '@directus/extensions';
+import type { EventContext } from '@directus/types';
+import type { RegisterFunctions } from '@directus/extensions';
 
-// src/index.ts
-// import { defineHook } from "@directus/extensions-sdk";
-import { deleteUnusedM2OItems } from "directus-hook-library";
+// export default defineHook((register, context: HookExtensionContext) => {
+//     deleteUnusedM2OItems(register, context, {
+//         oneCollection: "meta_infos",
+//         manyCollections: {
+//             pages: "pages",
+//             posts: "posts",
+//         },
+//     });
+// });
 
-export default defineHook((register, context) => {
-    deleteUnusedM2OItems(register, context, {
-        oneCollection: "meta_infos",
-        manyCollections: {
-            pages: "pages",
-            posts: "posts",
-        },
-    });
+type ManyField = string;
+
+type ManyCollection = string;
+
+// type O2MContext = {
+// 	/** collection that potentially have unused items */
+// 	oneCollection: string;
+// 	/** collections and their reverse key field, that are related to the `oneCollection` item */
+// 	manyCollections: {
+// 		[key: ManyField]: ManyCollection;
+// 	};
+// };
+
+export default defineHook(({ action }: RegisterFunctions, { services, getSchema }: HookExtensionContext) => {
+	action('items.update', handler);
+	action('items.delete', handler);
+	let oneCollection: string = 'meta_infos';
+	let manyCollections: { [key: ManyField]: ManyCollection } = {
+		pages: 'pages',
+		posts: 'posts',
+	};
+	async function handler(meta: Record<string, any>, context: EventContext) {
+		const _manyCollections = Object.values(manyCollections);
+		if (!_manyCollections.includes(meta.collection)) return;
+
+		const _manyFields = Object.keys(manyCollections);
+
+		const { ItemsService } = services;
+		const itemsService = new ItemsService(oneCollection, {
+			...context,
+			schema: await getSchema(),
+		});
+
+		await itemsService.deleteByQuery({
+			limit: -1,
+			filter: {
+				_and: _manyFields.map((field) => ({
+					[field]: { _null: true },
+				})),
+			},
+		});
+	}
 });
